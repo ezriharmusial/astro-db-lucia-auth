5c7a8c835f7faeffa14c894bdc7e1e7a1acb2bca
ezriharmusial <ezriharmusial@gmail.com>
2024-08-27 00:46:50 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        react(),
-        tailwind({
-            applyBaseStyles: false
-        })
-    ],
-    output: 'hybrid',
-    adapter: netlify()
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), db()],
+  output: 'hybrid',
+  adapter: netlify()
 });
